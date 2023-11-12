67338b4ba11e4211db20366c5414d3403a45dc76
mcs <maicongson69@gmail.com>
2023-11-12 20:09:18 +0700

    add: footer icon

--- src/constants/Icon.ts
+++ src/constants/Icon.ts
@@ -2,5 +2,40 @@ import LogoIcon from "@/assets/icons/logo.svg?react";
 import SearchIcon from "@/assets/icons/search.svg?react";
 import BagIcon from "@/assets/icons/bag.svg?react";
 import UserIcon from "@/assets/icons/user.svg?react";
+import LocationIcon from "@/assets/icons/location.svg?react";
+import PhoneIcon from "@/assets/icons/phone.svg?react";
+import MailIcon from "@/assets/icons/mail.svg?react";
+import FacebookIcon from "@/assets/icons/facebook.svg?react";
+import TwitterIcon from "@/assets/icons/twitter.svg?react";
+import InstagramIcon from "@/assets/icons/instagram.svg?react";
+import YoutubeIcon from "@/assets/icons/youtube.svg?react";
+import PaypalIcon from "@/assets/icons/paypal.svg?react";
+import AmericanExpressIcon from "@/assets/icons/american-express.svg?react";
+import VisaIcon from "@/assets/icons/visa.svg?react";
+import MasterCardIcon from "@/assets/icons/master-card.svg?react";
+import CopyrightIcon from "@/assets/icons/copyright.svg?react";
+import ArrowUpIcon from "@/assets/icons/arrow-up.svg?react";
+import ArrowRightIcon from "@/assets/icons/arrow-right.svg?react";
+import MessageQuestionIcon from "@/assets/icons/message-question.svg?react";
 
-export { LogoIcon, SearchIcon, BagIcon, UserIcon };
+export {
+  LogoIcon,
+  SearchIcon,
+  BagIcon,
+  UserIcon,
+  LocationIcon,
+  PhoneIcon,
+  MailIcon,
+  FacebookIcon,
+  TwitterIcon,
+  InstagramIcon,
+  YoutubeIcon,
+  PaypalIcon,
+  AmericanExpressIcon,
+  VisaIcon,
+  MasterCardIcon,
+  CopyrightIcon,
+  ArrowUpIcon,
+  ArrowRightIcon,
+  MessageQuestionIcon,
+};
