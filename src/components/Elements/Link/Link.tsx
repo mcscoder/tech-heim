553b4553c7a5036d94f9aa04e663238391e8d95b
mcs <maicongson69@gmail.com>
2023-11-14 00:33:17 +0700

    add: Link component

---
+++ src/components/Elements/Link/Link.tsx
@@ -0,0 +1,44 @@
+import {
+  Link as ReactLink,
+  LinkProps as ReactLinkProps,
+} from "react-router-dom";
+
+interface IconProps {
+  startIcon?: React.ReactNode;
+  endIcon?: React.ReactNode;
+}
+
+type LinkProps = Omit<ReactLinkProps, "to"> &
+  React.AnchorHTMLAttributes<HTMLAnchorElement> &
+  IconProps & {
+    children?: React.ReactNode;
+    to?: string;
+  };
+
+export const Link = ({
+  startIcon,
+  endIcon,
+  children,
+  to,
+  href,
+  className = "",
+  ...props
+}: LinkProps) => {
+  const Comp = to ? ReactLink : "a";
+  const linkClasses = `inline-flex items-center justify-center ${className} ${
+    children && "gap-1"
+  }`;
+
+  return (
+    <Comp
+      to={to || "#"}
+      href={href || "#"}
+      className={linkClasses}
+      {...props}
+    >
+      {startIcon && startIcon}
+      {children && <span>{children}</span>}
+      {endIcon && endIcon}
+    </Comp>
+  );
+};
