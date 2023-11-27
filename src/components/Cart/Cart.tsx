c6886274ebf5f1f3818f00853c600aaec5ad0933
mcs <maicongson69@gmail.com>
2023-11-27 23:45:30 +0700

    ref: add count component and fix related components

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -12,9 +12,21 @@ export const Cart = () => {
       <CartWrapper>
         <TotalItemCount quantity={3} />
         <ItemWrapper>
-          <CartItemWrapper />
-          <CartItemWrapper />
-          <CartItemWrapper />
+          <CartItemWrapper
+            imageURL="https://s3-alpha-sig.figma.com/img/da0e/b4b4/8976c397ec43f918ec3c69b360439260?Expires=1702252800&Signature=nKfmSa0HtNEGTyrYbNzg1hYE-QxRppNKoGNdU0H4d6HkTTGLV-vNaJIXduxBT30SWoIIa0rJ7GLjm2zUtYPDK2Dl9jbZ5mN7X-VRnBW-PhpKqm2GMzuIRhCN2FDgm5XvCbmkXkp1xkjrKzI4cEyd8dD~LRUkKfdHHelzR2LOaedGPTG~Uijln~gT5iPyAJzlOLUBlQ4rPOndfa2f2BfGhMu4Aj6-cfr8S41WYXLef5HSY-tKNzSW7OAplqo1EwmOH~rU2toCz4iZDhcrlx5Y52cdQOqMp5zBGzGeRSyETEtdyg3V41cYjVMCy2ZeqszhBln5ud~GafZXh~6RhugqCw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+            title="MacBook Pro M2 MNEJ3 2022 LLA 13.3 inch"
+            currentPrice={433}
+          />
+          <CartItemWrapper
+            imageURL="https://s3-alpha-sig.figma.com/img/da0e/b4b4/8976c397ec43f918ec3c69b360439260?Expires=1702252800&Signature=nKfmSa0HtNEGTyrYbNzg1hYE-QxRppNKoGNdU0H4d6HkTTGLV-vNaJIXduxBT30SWoIIa0rJ7GLjm2zUtYPDK2Dl9jbZ5mN7X-VRnBW-PhpKqm2GMzuIRhCN2FDgm5XvCbmkXkp1xkjrKzI4cEyd8dD~LRUkKfdHHelzR2LOaedGPTG~Uijln~gT5iPyAJzlOLUBlQ4rPOndfa2f2BfGhMu4Aj6-cfr8S41WYXLef5HSY-tKNzSW7OAplqo1EwmOH~rU2toCz4iZDhcrlx5Y52cdQOqMp5zBGzGeRSyETEtdyg3V41cYjVMCy2ZeqszhBln5ud~GafZXh~6RhugqCw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+            title="MacBook Pro M2 MNEJ3 2022 LLA 13.3 inch"
+            currentPrice={433}
+          />
+          <CartItemWrapper
+            imageURL="https://s3-alpha-sig.figma.com/img/da0e/b4b4/8976c397ec43f918ec3c69b360439260?Expires=1702252800&Signature=nKfmSa0HtNEGTyrYbNzg1hYE-QxRppNKoGNdU0H4d6HkTTGLV-vNaJIXduxBT30SWoIIa0rJ7GLjm2zUtYPDK2Dl9jbZ5mN7X-VRnBW-PhpKqm2GMzuIRhCN2FDgm5XvCbmkXkp1xkjrKzI4cEyd8dD~LRUkKfdHHelzR2LOaedGPTG~Uijln~gT5iPyAJzlOLUBlQ4rPOndfa2f2BfGhMu4Aj6-cfr8S41WYXLef5HSY-tKNzSW7OAplqo1EwmOH~rU2toCz4iZDhcrlx5Y52cdQOqMp5zBGzGeRSyETEtdyg3V41cYjVMCy2ZeqszhBln5ud~GafZXh~6RhugqCw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+            title="MacBook Pro M2 MNEJ3 2022 LLA 13.3 inch"
+            currentPrice={433}
+          />
         </ItemWrapper>
         <CartSummary />
       </CartWrapper>
--- src/components/Cart/components/CartItemWrapper.tsx
+++ src/components/Cart/components/CartItemWrapper.tsx
@@ -1,27 +1,34 @@
-import { Button, IconText } from "@/components/Elements";
-import {
-  DeliveryIcon,
-  GuaranteedIcon,
-  MinusIcon,
-  PlusIcon,
-  TrashIcon,
-} from "@/constants";
+import { Count, IconText } from "@/components/Elements";
+import { DeliveryIcon, GuaranteedIcon } from "@/constants";
+import { formatUSD } from "@/utils";
+import { useState } from "react";
 
-export const CartItemWrapper = () => {
+export interface CartItemWrapperProps {
+  title: string;
+  imageURL: string;
+  currentPrice: number;
+}
+
+export const CartItemWrapper = ({
+  title,
+  imageURL,
+  currentPrice,
+}: CartItemWrapperProps) => {
+  const [quantity, setQuantity] = useState<number>(1);
+
   return (
     <div className="flex gap-4 rounded-lg shadow-1 p-4">
       <div className="flex-[2]">
         <img
-          src="https://s3-alpha-sig.figma.com/img/da0e/b4b4/8976c397ec43f918ec3c69b360439260?Expires=1701043200&Signature=PE~2jqgoVyrCmcbadGT5odxwXVn1vTj16P5Rebyy9uRSGsaWBHQNH8gwVcvmdadyLZwuLekyYxvT65We6CxcpdRoFqKMPhQzOcdoBrOFz3gE0z2pQmw1kw-yau2BiKfZjbbB5LZ7Lm69p7OgvIU7cdXvpoUh9QzWyEXFhfwik8NRcmwr2LQNZeaHlFBvUDrNImSISG38XPicmaghbkFNhu~CeqhUKrPU4IS1EpSHbx1qE~cgdk-ndlW1VckfNGggORMj~sKl6W-GANacfgSW-LLCMiPlygImYuxjZaIvll2pOFYgA0tv~Hy2t2HxTWf35jH-76NH~-4pmPS0GiBCGg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+          src={imageURL}
           alt=""
         />
       </div>
       <div className="flex-[3] flex flex-col gap-2">
         <div className="flex flex-col gap-1">
-          <h6>MacBook Pro M2 MNEJ3 2022 LLA 13.3 inch</h6>
+          <h6>{title}</h6>
           <div className="flex flex-col">
-            <p className="font-caption-sm text-gray-71">Black</p>
-            <p className="font-caption-sm text-gray-71">x1</p>
+            <p className="font-caption-sm text-gray-71">x{quantity}</p>
           </div>
         </div>
         <div className="flex flex-col gap-1">
@@ -39,24 +46,13 @@ export const CartItemWrapper = () => {
           </IconText>
         </div>
         <div className="flex items-center justify-between">
-          <p className="font-body-md text-gray-d2d">$433333.00</p>
-          <div className="flex items-center justify-center gap-1">
-            <Button
-              variant="onlyIcon"
-              startIcon={<TrashIcon />}
-            />
-            <div className="flex items-center gap-3 border-b-[1px] border-b-gray-e9e">
-              <Button
-                variant="onlyIcon"
-                startIcon={<MinusIcon />}
-              />
-              <span>1</span>
-              <Button
-                variant="onlyIcon"
-                startIcon={<PlusIcon />}
-              />
-            </div>
-          </div>
+          <p className="font-body-md text-gray-d2d">
+            {formatUSD(currentPrice * quantity)}
+          </p>
+          <Count
+            quantity={quantity}
+            setQuantity={setQuantity}
+          />
         </div>
       </div>
     </div>
