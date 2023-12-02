6dc94ba08ad1f48c465ced67ba8cd3e06d81bd3e
mcs <maicongson69@gmail.com>
2023-12-02 15:53:27 +0700

    ref: refactor product filter to handle search param and fit to api

--- src/components/Elements/ProductFilter/FilterGroup.tsx
+++ src/components/Elements/ProductFilter/FilterGroup.tsx
@@ -3,11 +3,12 @@ import { useState } from "react";
 import { Filter, FilterProps } from ".";
 
 export interface ProductFilterProps {
+  id: number;
   title: string;
-  filters: FilterProps[];
+  productType: FilterProps[];
 }
 
-export const FilterGroup = ({ title, filters }: ProductFilterProps) => {
+export const FilterGroup = ({ title, productType }: ProductFilterProps) => {
   const [expanded, setExpanded] = useState<boolean>(false);
 
   const handleExpanding = () => {
@@ -25,11 +26,11 @@ export const FilterGroup = ({ title, filters }: ProductFilterProps) => {
       </button>
       {expanded && (
         <div className="flex flex-col items-start gap-4 pb-8 px-4">
-          {filters.map((filter, index) => {
+          {productType.map((type, index) => {
             return (
               <Filter
                 key={index}
-                {...filter}
+                {...type}
               />
             );
           })}
