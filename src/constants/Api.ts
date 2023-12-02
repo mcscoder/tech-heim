26f6b3a380583ebf25d9e009a38696b29934a342
mcs <maicongson69@gmail.com>
2023-12-02 16:14:59 +0700

    add: product group end point

--- src/constants/Api.ts
+++ src/constants/Api.ts
@@ -1,9 +1,11 @@
-export const apiURL = "http://any:3000";
+export const apiURL =
+  "https://43fa-2001-ee0-4b6e-cf80-414e-301c-410-d1bd.ngrok-free.app";
 export const apiEndPoint = {
   users: "/users",
   homeCategories: "/home-categories",
   productSales: "/product-sales",
   productNews: "/product-news",
   productBestSellers: "/product-best-sellers",
+  productGroup: "/productGroup",
 };
 export type ApiEndPointTypes = keyof typeof apiEndPoint;
