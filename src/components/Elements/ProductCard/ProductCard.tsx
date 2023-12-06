1f864f5e4634badf11636f6f2600e605835ea2e1
mcs <maicongson69@gmail.com>
2023-12-06 16:56:49 +0700

    ref: add more state

--- src/components/Elements/ProductCard/ProductCard.tsx
+++ src/components/Elements/ProductCard/ProductCard.tsx
@@ -1,6 +1,6 @@
-import { StarIcon } from "@/constants";
+import { CartIcon, HeartIcon, StarIcon, TrashIcon } from "@/constants";
 import { Link } from "react-router-dom";
-import { IconText, PercentDiscount } from "..";
+import { Button, IconText, PercentDiscount } from "..";
 import { formatUSD } from "@/utils";
 
 export interface ProductCardApi {
@@ -14,6 +14,9 @@ export interface ProductCardApi {
 
 type ProductCardProps = ProductCardApi & {
   to: string;
+  inWishList?: boolean;
+  handleAddToCart?: () => void;
+  handleWishItem?: () => void;
 };
 
 export const ProductCard = ({
@@ -24,45 +27,73 @@ export const ProductCard = ({
   currentPrice,
   rate,
   to,
+  inWishList = false,
 }: ProductCardProps) => {
   console.log(imageURLs[0].imageURL);
   return (
-    <Link
-      to={to}
-      className="group p-4 bg-white rounded-lg shadow-1 hover:shadow-2 hover:scale-105 duration-100 flex flex-col gap-4 border border-transparent hover:border-primary-75 relative"
-    >
-      {lastPrice && (
-        <PercentDiscount
-          className="absolute top-2 left-0"
-          lastPrice={lastPrice}
-          currentPrice={currentPrice}
-        />
-      )}
-      <div className="border-b group-hover:border-b-primary-75 pb-4 duration-100">
-        <img
-          src={imageURLs[0].imageURL}
-          alt={name}
-        />
-      </div>
-      <p className="font-body-md group-hover:text-Primary line-clamp-2 duration-100">
-        {name}
-      </p>
-      <div className="flex items-end justify-between">
-        <div className="flex flex-col items-start">
-          {/* last price */}
-          {lastPrice && (
-            <p className="text-gray-71 line-through">{formatUSD(lastPrice)}</p>
-          )}
-          {/* current price */}
-          <p className="font-body-lg">{formatUSD(currentPrice)}</p>
+    <div className="flex flex-col group hover:scale-105 duration-100 relative hover:z-50">
+      <Link
+        to={to}
+        className="flex flex-col gap-4 relative p-4"
+      >
+        {lastPrice && (
+          <PercentDiscount
+            className="absolute top-2 left-0"
+            lastPrice={lastPrice}
+            currentPrice={currentPrice}
+          />
+        )}
+        <div className="border-b group-hover:border-b-primary-75 pb-4 duration-100">
+          <img
+            src={imageURLs[0].imageURL}
+            alt={name}
+          />
         </div>
-        <IconText
-          startIcon={<StarIcon />}
-          className="text-[16px] font-medium"
-        >
-          {rate}
-        </IconText>
+        <p className="font-body-md group-hover:text-Primary line-clamp-2 duration-100">
+          {name}
+        </p>
+        <div className="flex items-end justify-between">
+          <div className="flex flex-col items-start">
+            {/* last price */}
+            {lastPrice && (
+              <p className="text-gray-71 line-through">
+                {formatUSD(lastPrice)}
+              </p>
+            )}
+            {/* current price */}
+            <p className="font-body-lg">{formatUSD(currentPrice)}</p>
+          </div>
+          <IconText
+            startIcon={<StarIcon />}
+            className="text-[16px] font-medium"
+          >
+            {rate}
+          </IconText>
+        </div>
+      </Link>
+      <div className="absolute top-0 bottom-0 group-hover:-bottom-14 left-0 right-0 bg-white rounded-lg shadow-1 group-hover:shadow-2 border border-transparent group-hover:border-primary-75 -z-10 flex items-end">
+        <div className="hidden group-hover:flex items-center justify-between p-4 flex-1">
+          <Button
+            variant="outlined"
+            startIcon={<CartIcon />}
+          >
+            Add to cart
+          </Button>
+          <Button
+            variant="onlyIcon"
+            startIcon={
+              inWishList ? (
+                <TrashIcon
+                  width={24}
+                  height={24}
+                />
+              ) : (
+                <HeartIcon />
+              )
+            }
+          />
+        </div>
       </div>
-    </Link>
+    </div>
   );
 };
