5295eb16ca7a28914fb36ae6d6fae83e00b97614
mcs <maicongson69@gmail.com>
2023-12-12 01:29:12 +0700

    add: scroll page to the top after route switched

--- src/components/Layouts/DefaultLayout/DefaultLayout.tsx
+++ src/components/Layouts/DefaultLayout/DefaultLayout.tsx
@@ -1,8 +1,15 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import { Header } from "..";
 import { Footer } from "../Footer";
+import { useEffect } from "react";
 
 export const DefaultLayout = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scroll(0, 0);
+  }, [pathname]);
+
   return (
     <div className="flex flex-col h-screen">
       <Header />
